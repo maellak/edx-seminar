5da481f17eaaf563cfe1297ef8695b9a2b06eccb
Achilleas Pipinellis <axilleas@axilleas.me>
2015-04-02 02:36:34 +0300

    Split into roles.

---
+++ dev/okeanos/roles/postinstall/files/mongo_fix.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+service mongod stop
+
+if [ -f /edx/var/mongo/mongodb/mongof.lock ]
+  rm -f /edx/var/mongo/mongodb/mongod.lock
+fi
+
+if [ -d /edx/var/mongo/mongodb/admin ]
+  rm -rf /edx/var/mongo/mongodb/admin
+fi
+
+if [ -d /edx/var/mongo/mongodb/local ]
+  rm -rf /edx/var/mongo/mongodb/local
+fi
+
+mongod --repair --config /etc/mongodb.conf
+
+service start mongod
+
+chown -R mongodb:mongodb /edx/var/mongo/
